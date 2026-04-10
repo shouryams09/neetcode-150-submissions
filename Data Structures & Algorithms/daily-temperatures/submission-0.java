0015b5344b85955e10b6b0e3c03ecac241f9bbbd
shouryams09 <shouryams09@gmail.com>
2026-04-11 01:54:07 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.java
@@ -0,0 +1,35 @@
+class Pair<T,V>{
+    public T first;
+    public V second;
+
+    public Pair(T first,V second){
+        this.first=first;
+        this.second=second;
+    }
+}
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int x=temperatures.length-1;
+        int a[]=new int[temperatures.length];
+        Stack<Pair<Integer,Integer>> s=new Stack<>();
+        for(int i=temperatures.length-1;i>=0;i--){
+            if(s.isEmpty()){
+                a[x--]=0;
+            }else if(s.peek().first>temperatures[i]){
+                a[x--]=Math.abs(s.peek().second-i);
+            }else if(s.peek().first<=temperatures[i]){
+                while(!s.isEmpty() && s.peek().first<=temperatures[i]){
+                    s.pop();
+                }
+                if(s.isEmpty()){
+                    a[x--]=0;
+                }else{
+                    a[x--]=Math.abs(s.peek().second-i);
+                }
+            }
+            s.push(new Pair(temperatures[i],i));
+        }
+
+        return a;
+    }
+}
